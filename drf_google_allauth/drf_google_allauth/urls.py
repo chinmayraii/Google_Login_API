c339061f5bd88b668a643f23d011d8e38856d3ca
chinmayraii <chinmayrai75@gmail.com>
2023-04-19 16:02:06 +0530

    update

---
+++ drf_google_allauth/drf_google_allauth/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from app.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # path('', include('app.urls')),
+    path('api/auth/', include('rest_framework.urls')),
+    path('auth/', include('drf_social_oauth2.urls', namespace='drf'))
+
+]
